deb8b57a4cf7fe3337320e04d38cb3ad38be4dad
chriseikelboom <ceikelboom@sp.nl>
2018-02-23 15:31:56 +0100

    Ignore street and address already being filled
    It should be overwritten.

--- js/sp_webformautoaddress.js
+++ js/sp_webformautoaddress.js
@@ -10,16 +10,12 @@
 
       // Set the street name when the user hasn't filled in anything yet. To
       // prevent the user adding to the text field, lose its (possible) focus.
-      if (!$("input.ajax-callback-street-name--" + data.formKey).val()) {
-        $("input.ajax-callback-street-name--" + data.formKey).blur();
-        $("input.ajax-callback-street-name--" + data.formKey).val(data.street);
-      }
+      $("input.ajax-callback-street-name--" + data.formKey).blur();
+      $("input.ajax-callback-street-name--" + data.formKey).val(data.street);
 
       // Set the city when the user hasn't filled in anything yet. To
       // prevent the user adding to the text field, lose its (possible) focus.
-      if (!$("input.ajax-callback-city--" + data.formKey).val()) {
-        $("input.ajax-callback-city--" + data.formKey).blur();
-        $("input.ajax-callback-city--" + data.formKey).val(data.city);
-      }
-    }
+      $("input.ajax-callback-city--" + data.formKey).blur();
+      $("input.ajax-callback-city--" + data.formKey).val(data.city);
+    };
 })(jQuery, Drupal, this, this.document);
