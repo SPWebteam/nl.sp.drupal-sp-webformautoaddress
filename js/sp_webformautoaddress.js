b2a0d73505fdb74d8524c14e7ba48bed85aac8f4
chriseikelboom <ceikelboom@sp.nl>
2020-12-23 14:56:09 +0100

    Javascript update for postal code field.
    Conflicts:
    	js/sp_webformautoaddress.js

--- js/sp_webformautoaddress.js
+++ js/sp_webformautoaddress.js
@@ -4,6 +4,34 @@
  */
 
 (function ($, Drupal, window, document, undefined) {
+
+  $(document).ready(function(){
+
+    // if you have defined a coupled postcode field (wrapper gets class postcode-coupled)
+    // keep that one in sync with the automcomplete postcode field
+      if($('.postcode-coupled input').length) {
+        $('.postcode-coupled input').on('change',function(){
+          var coupledPC = $(this).val();
+          if(coupledPC) {
+            $('#edit-submitted-adres-postal-code').val(coupledPC);
+          }
+        });
+        $('#edit-submitted-adres-postal-code').on('change',function(){
+          var coupledPC = $(this).val();
+          if(coupledPC) {
+            $('.postcode-coupled input').val(coupledPC);
+          }
+        });
+      }
+      $('input.toggle-pcc').on('change', function(){
+        if ($('input.toggle-pcc').prop('checked')) {
+          $('.postcode-coupled').hide();
+        } else {
+          $('.postcode-coupled').show();
+        }
+      });
+  });
+
     $.fn.SPwebformAutoAddressInject = function (arguments) {
       // Parse the JSON argument.
       var data = JSON.parse(arguments);
